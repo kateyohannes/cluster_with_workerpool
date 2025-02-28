8a946ad8592b0aff997fa98ba432fc30b9d37998
Yohannes Teshome <kateyohannes@gmail.com>
2025-03-01 00:05:14 +0300

    use cluster

--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
 'use strict';
 
-import { createServer } from 'node:http'
-import app from './app.js';
+import os from 'node:os'
+import cluster from 'node:cluster';
+import process from 'node:process';
+import { createServer } from 'node:http';
 
-const server = createServer(app);
-const port = process.env.PORT || 3000;
+import app from './app.js'
 
-server.listen(port, ()=>{
-    console.log(`Server is on http://localhost:${port}`);
-});
+const cpu_count = os.cpus().length
+// const cpu_length = 2
+
+if (cluster.isPrimary) {
+    console.log(`cluster: total cpu_count ${cpu_count}`)
+    for (let i = 0; i < cpu_count; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    const port = process.env.PORT_WC || 3000;
+    const server = createServer(app);
+
+    server.listen(port, () => {
+        console.log(`Server is on http://localhost:${port}`);
+    });
+
+
+    console.log(`Worker ${process.pid} started`);
+}
+
