5c0fa769aad1a560e6a8fed8c58ef0610de3fabe
Yohannes Teshome <kateyohannes@gmail.com>
2025-03-04 18:22:12 +0300

    add morgan for logging and register user route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
+import morgan from 'morgan';
 import express from 'express';
 import fibonacciRoute from './routes/fibonacci.route.js';
+import userRouter from './routes/user.route.js'
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 
 app.use('/', fibonacciRoute);
-
+app.use('/users', userRouter);
 export default app;
