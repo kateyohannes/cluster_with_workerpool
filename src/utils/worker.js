1f13d324f3cfb221b11e90c7dc12ebea1455439c
Yohannes Teshome <kateyohannes@gmail.com>
2025-03-04 18:25:35 +0300

    register user services

--- src/utils/worker.js
+++ src/utils/worker.js
@@ -1,6 +1,16 @@
-import workerpool from "workerpool";
-import fibonacci from "./fibonacci.js"
+import workerpool from 'workerpool';
+import fibonacci from './fibonacci.js';
+import load_csv from './csv.js';
+import {
+    getAllUsersService,
+    getOneUserService,
+    insertUsersFromCsvFileService
+} from '../services/user.service.js';
 
 workerpool.worker({
-    fibonacci
+    load_csv,
+    fibonacci,
+    getOneUserService,
+    getAllUsersService,
+    insertUsersFromCsvFileService
 });
